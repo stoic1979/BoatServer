593993379f2151de0ea0f119cd9b1a0db8b75f4e
Navjot Singh <weavebytes@gmail.com>
2017-03-07 23:11:38 +0530

    bare bones

---
+++ models.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///boatdb.sqlite'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(60), unique=True)
+    email = db.Column(db.String(60), unique=True)
+    phone = db.Column(db.String(16), unique=True)
+
+    def __init__(self, username, email, phone):
+        self.username = username
+        self.email = email
+        self.phone = phone
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+db.create_all()
+
+if __name__ == "__main__":
+    """
+    admin = User('admin', 'admin@example.com', "99999999")
+    guest = User('guest', 'guest@example.com', "77777777")
+
+    db.session.add(admin)
+    db.session.add(guest)
+    db.session.commit()
+    """
+    print "Users:",  User.query.all()
