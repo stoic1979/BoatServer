295f20beb45a53107c7e56588a9ff44e0c70a3c3
Navjot Singh <weavebytes@gmail.com>
2017-04-18 19:42:33 +0530

    use threaded for server run

--- server.py
+++ server.py
@@ -470,4 +470,4 @@ def get_police_boat_locations():
 if __name__ == "__main__":
     #db.create_all()
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
